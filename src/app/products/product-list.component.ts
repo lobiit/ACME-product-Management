7d82b668957ff7c51c33dab72dddc2e9aaad481b
Tumbo Lobi John <johnlobiit@gmail.com>
2021-10-22 17:49:50 +0300

    added list filtering

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from "./product";
 
 @Component ({
@@ -6,12 +6,23 @@ import {IProduct} from "./product";
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false ;
-    listFilter: string = 'cart';
+    private _listFilter: string = '';
+    get listFilter(): string {
+      return this._listFilter
+    }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      console.log('In setter:', value);
+      this.filteredProducts = this.performFilter(value)
+    }
+
+    filteredProducts: IProduct[] = [];
+
     products: IProduct[] = [
       {
         "productId": 1,
@@ -34,7 +45,18 @@ export class ProductListComponent {
         "imageUrl": "assets/images/garden_cart.png"
       }
       ];
+    performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLowerCase();
+      return this.products.filter((product: IProduct)=>product.productName.toLowerCase().includes(filterBy));
+    }
     toggleImage(): void {
       this.showImage = !this.showImage;
     }
+
+    ngOnInit(): void {
+      this.listFilter = 'cart';
+  }
 }
+
+
+
